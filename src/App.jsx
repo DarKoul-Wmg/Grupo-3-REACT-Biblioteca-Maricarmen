2046d9d050d41b1a834d43ef7a7bd0fac601e7bc
William Molina <williammg0904@gmail.com>
2025-04-09 19:38:38 +0200

    [3] - Importació massiva (FRONTEND)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import BookList from './components/BookList';
 import './styles.css';
+import InputCsv from './components/InputCsv';
 
 function App() {
   return (
@@ -12,3 +13,4 @@ function App() {
 }
 
 export default App;
+
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
-const API_URL = 'http://localhost:8000/api/llibres'; // Ajusta según tu Django API
+const API_URL = 'http://localhost:8000/api/'; // Ajusta según tu Django API
 
 export const getBooks = () => {
   console.log('llamando API...');
-  return fetch(API_URL)
+  return fetch(API_URL + 'llibres')
     .then((response) => {
       if (!response.ok) {
         throw new Error('Error al obtener los libros');
@@ -14,3 +14,27 @@ export const getBooks = () => {
       return [];
     });
 };
+
+
+ /* Obtenemos el archivo CSV para ser exportado en la base de datos*/
+export const importCsv = (file) => {
+    console.log('Llamada a la API para importar CSV...');
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return fetch(API_URL + 'importcsv', {
+      method: 'POST',
+      body:FormData,
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Error al importar el CSV');
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.error('Error en la API:', error);
+      return [];
+    });
+}
