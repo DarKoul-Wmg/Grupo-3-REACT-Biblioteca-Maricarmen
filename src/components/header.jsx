543ad953dcbc837cc86cbac151974e781641e89f
Erikpr04 <erikpr32@gmail.com>
2025-04-10 12:09:51 +0200

    Header style fixes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -15,7 +15,7 @@ export default function Header({
 
   return (
     <header className="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full bg-white border-b border-gray-200">
-      <nav className="flex items-center justify-between relative max-w-[85rem] w-full mx-auto py-5 px-4 sm:px-6 lg:px-8">
+      <nav className="flex items-center justify-between relative w-full mx-auto py-5 px-4 sm:px-6 lg:px-8">
         <Link href="/" className="block">
           Biblioteca Mari Carmen
         </Link>
